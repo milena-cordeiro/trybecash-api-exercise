d6974ef4c32f7a3937f3e4691faf86efd3d70a67
Milena Cordeiro <mcs9823@gmail.com>
2023-07-19 16:39:22 -0300

    anotacoes

--- src/db/connection.js
+++ src/db/connection.js
@@ -1,4 +1,4 @@
-// src/db/connection.js
+// No trecho de código estamos importando a biblioteca mysql2 com o recurso de promises. Isso permitirá utilizar o MySQL de forma assíncrona com async/await. Em seguida, criamos uma constante connection que recebe um pool de conexões criado com a função createPool.
 
 const mysql = require('mysql2/promise');
 
